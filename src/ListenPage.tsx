27cf3b1e1bba59760e5594f60872ce0f8d794954
Olivia Brown <obrown3@oberlin.edu>
2025-06-01 09:40:48 -0700

    Fixed session storage

--- src/ListenPage.tsx
+++ src/ListenPage.tsx
@@ -41,7 +41,7 @@ const ListenPage: React.FC = () => {
         onClick={(e) => {
           e.stopPropagation();
           e.currentTarget.blur();
-          localStorage.setItem("hasEntered", "true");
+          sessionStorage.setItem("hasEntered", "true");
           navigate("/");
         }}
         onMouseEnter={(e) => {
